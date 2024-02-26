c242d91ed3b95e62e8b63777a8e2fcdf35726fcc
Juliandresval <juanval1986@gmail.com>
2024-02-26 16:05:03 -0500

    Avance auth

--- src/Auth/Infrastructure/Controller/ApiLoginController.php
+++ src/Auth/Infrastructure/Controller/ApiLoginController.php
@@ -6,15 +6,27 @@ namespace App\Auth\Infrastructure\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Auth\Domain\Entity\User;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class ApiLoginController extends AbstractController
 {
-    #[Route('/api/login', name: 'api_login')]
-    public function index(): JsonResponse
+    #[Route('/api/login', name: 'api_login', methods: 'POST')]
+    public function index(#[CurrentUser] ?User $user): JsonResponse
     {
+        if (null === $user) {
+            return $this->json([
+                'message' => 'missing credentials',
+            ], JsonResponse::HTTP_UNAUTHORIZED);
+        }
+
+        $token = "jwt";
+
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/ApiLoginController.php',
+            'user'  => $user->getUserIdentifier(),
+            'token' => $token,
         ]);
     }
 }
